f818286c8aafb5ae8e4e7197c11c672453d3c07d
WKJYES <iwkj@foxmail.com>
2022-12-21 21:41:10 +0800

    修复查找返回不是List问题

--- src/main/java/com/example/carbonfootprintcalculator/controller/ItemController.java
+++ src/main/java/com/example/carbonfootprintcalculator/controller/ItemController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @Service
 @RequestMapping("/item")
@@ -27,7 +30,7 @@ public class ItemController {
     }
 
     @PostMapping("/find")
-    public Item find(@RequestBody User user) {
+    public List<Item> find(@RequestBody User user) {
         return itemMapper.findByUserName(user.getUname());
     }
 
--- src/main/java/com/example/carbonfootprintcalculator/mapper/ItemMapper.java
+++ src/main/java/com/example/carbonfootprintcalculator/mapper/ItemMapper.java
@@ -7,34 +7,36 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Mapper
 @Repository
 public interface ItemMapper {
     /**
      * 插入
      */
-    @Update("insert into t_item values(#{itemName},#{carbonNum},#{date},#{uname})")
+    @Update("insert into t_item values(#{carbonNum},#{date},#{uname})")
     void insert(Item item);
 
     /**
      * 更新
      */
-    @Update("update t_item set carbonNum = #{carbonNum} where itemName = #{itemName} and uname = #{uname} and date = #{date}")
+    @Update("update t_item set carbonNum = #{carbonNum} where uname = #{uname} and date = #{date}")
     void update(Item item);
 
     /**
      * 删除
      */
-    @Delete("delete  from t_item where itemName = #{itemName} and uname = #{uname} and date = #{date}")
+    @Delete("delete  from t_item where uname = #{uname} and date = #{date}")
     void delete(Item item);
 
     /**
      * 查询
      */
-//注册时判断物品是否存在
-    @Select("select* from t_item where itemName = #{itemName}")
-    Item insertByName(String name);
+////注册时判断物品是否存在
+//    @Select("select* from t_item where itemName = #{itemName}")
+//    Item insertByName(String name);
 
     @Select("select* from t_item where uname = #{uname}")
-    Item findByUserName(String uname);
+    List<Item> findByUserName(String uname);
 }
