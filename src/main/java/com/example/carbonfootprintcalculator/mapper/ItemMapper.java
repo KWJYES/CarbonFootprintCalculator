4dc62dff60849497c4e21f74e440055a6481f642
moon12306 <2836167734@qq.com>
2022-12-22 15:58:30 +0800

    在item中增加了树的计算并在插入前计算结果，再插入数据库.

--- src/main/java/com/example/carbonfootprintcalculator/mapper/ItemMapper.java
+++ src/main/java/com/example/carbonfootprintcalculator/mapper/ItemMapper.java
@@ -15,7 +15,7 @@ public interface ItemMapper {
     /**
      * 插入
      */
-    @Update("insert into t_item values(#{carbonNum},#{date},#{uname}，#{treeName})")
+    @Update("insert into t_item values(#{carbonNum},#{date},#{uname}，#{treeNum})")
     void insert(Item item);
 
     /**
